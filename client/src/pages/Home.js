a9d2dd6dbb97a05a790600e475ae89f8c2e2446f
Syed Muhammad <61853065+smnaqvidr@users.noreply.github.com>
2023-07-13 00:17:12 -0700

    first commit

---
+++ client/src/pages/Home.js
@@ -0,0 +1,107 @@
+import React, { useContext } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import ThumbUpOffAltIcon from '@mui/icons-material/ThumbUpOffAlt';
+import { AuthContext } from "../helpers/AuthContext";
+
+function Home() {
+  const [listOfPosts, setListOfPosts] = useState([]);
+  const [likedPosts, setLikedPosts] = useState([]);
+  const { authState } = useContext(AuthContext);
+  let history = useHistory();
+
+  useEffect(() => {
+    if (!localStorage.getItem("accessToken")) {
+      history.push("/login");
+    } else {
+      axios
+        .get("https://full-stack-api-app-5e30d50475c2.herokuapp.com/posts", {
+          headers: { accessToken: localStorage.getItem("accessToken") },
+        })
+        .then((response) => {
+          setListOfPosts(response.data.listOfPosts);
+          setLikedPosts(
+            response.data.likedPosts.map((like) => {
+              return like.PostId;
+            })
+          );
+        });
+    }
+  }, []);
+
+  const likeAPost = (postId) => {
+    axios
+      .post(
+        "https://full-stack-api-app-5e30d50475c2.herokuapp.com/likes",
+        { PostId: postId },
+        { headers: { accessToken: localStorage.getItem("accessToken") } }
+      )
+      .then((response) => {
+        setListOfPosts(
+          listOfPosts.map((post) => {
+            if (post.id === postId) {
+              if (response.data.liked) {
+                return { ...post, Likes: [...post.Likes, 0] };
+              } else {
+                const likesArray = post.Likes;
+                likesArray.pop();
+                return { ...post, Likes: likesArray };
+              }
+            } else {
+              return post;
+            }
+          })
+        );
+
+        if (likedPosts.includes(postId)) {
+          setLikedPosts(
+            likedPosts.filter((id) => {
+              return id != postId;
+            })
+          );
+        } else {
+          setLikedPosts([...likedPosts, postId]);
+        }
+      });
+  };
+
+  return (
+    <div>
+      {listOfPosts.map((value, key) => {
+        return (
+          <div key={key} className="post">
+            <div className="title"> {value.title} </div>
+            <div
+              className="body"
+              onClick={() => {
+                history.push(`/post/${value.id}`);
+              }}
+            >
+              {value.postText}
+            </div>
+            <div className="footer">
+              <div className="username">
+                <Link to={`/profile/${value.UserId}`}> {value.username} </Link>
+              </div>
+              <div className="buttons">
+                <ThumbUpOffAltIcon
+                  onClick={() => {
+                    likeAPost(value.id);
+                  }}
+                  className={
+                    likedPosts.includes(value.id) ? "unlikeBttn" : "likeBttn"
+                  }
+                />
+
+                <label> {value.Likes.length}</label>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default Home;
